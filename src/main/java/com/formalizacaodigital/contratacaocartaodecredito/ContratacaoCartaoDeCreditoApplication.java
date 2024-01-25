ec9ac0b3f4939e32e81779d4aac91d4a0e08e856
Otávio de Castro <otavio.vn01@hotmail.com>
2024-01-25 16:00:29 -0300

    Estrutura do projeto

---
+++ src/main/java/com/formalizacaodigital/contratacaocartaodecredito/ContratacaoCartaoDeCreditoApplication.java
@@ -0,0 +1,13 @@
+package com.formalizacaodigital.contratacaocartaodecredito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContratacaoCartaoDeCreditoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContratacaoCartaoDeCreditoApplication.class, args);
+	}
+
+}
